11f7519db378fb56c7fa419ba3faf4e5f07f0bda
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-04-14 22:16:51 -0400

    review layout

--- src/main/java/com/demoSpring/quickstart/QuickstartApplication.java
+++ src/main/java/com/demoSpring/quickstart/QuickstartApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuickstartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuickstartApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(QuickstartApplication.class, args);
+    }
 
 }
